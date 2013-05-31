b9989d5be2034e9e3f925ee9ff7de8e9eaeaa578
zhangyunlong <zhangyunlong@baidu.com>
2013-05-31 19:17:14 +0800

    添加默认配置

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ var minify = require('html-minifier').minify;
 module.exports = function(content, file, conf){
     return minify(content, conf);
 };
+
+module.exports.conf = {
+    removeCommentsFromCDATA : true,
+    collapseWhitespace : true,
+    collapseBooleanAttributes : true,
+    removeAttributeQuotes : true,
+    removeRedundantAttributes : true,
+    useShortDoctype : true,
+    removeEmptyAttributes : true,
+    removeOptionalTags : true,
+    removeScriptTypeAttributes : true,
+    removeStyleLinkTypeAttributes : true
+};
